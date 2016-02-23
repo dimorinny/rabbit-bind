9e638f802904c00070f0339b5445c0820e180e69
Dimorinny <didika914@gmail.com>
2016-02-24 01:36:40 +0300

    new api

--- rabbit_bind/rabbit_bind.py
+++ rabbit_bind/rabbit_bind.py
@@ -3,6 +3,21 @@
 import pika
 
 
+class Client(object):
+    def __init__(self, channel, routing_key, method, no_ack):
+        self._channel = channel
+        self._routing_key = routing_key
+        self._method = method
+        self._no_ack = no_ack
+
+    def send(self, data=None):
+        if data:
+            self._channel.basic_publish(exchange='', routing_key=self._routing_key, body=data)
+
+        if not self._no_ack:
+            self._channel.basic_ack(delivery_tag=self._method.delivery_tag)
+
+
 class RabbitBinder(object):
     def __init__(self, host, no_ack=False, durable=False, prefetch_count=0):
         self._no_ack = no_ack
@@ -16,12 +31,8 @@ class RabbitBinder(object):
         self._channel.queue_declare(queue=output_queue, durable=self._durable)
 
         def _handler(ch, method, properties, body):
-            result = handler(body.decode('utf-8'))
-            if result:
-                self._channel.basic_publish(exchange='', routing_key=output_queue, body=result)
-
-            if not self._no_ack:
-                ch.basic_ack(delivery_tag=method.delivery_tag)
+            client = Client(self._channel, output_queue, method, self._no_ack)
+            handler(body.decode('utf-8'), client)
 
         self._channel.basic_consume(_handler, queue=input_queue, no_ack=self._no_ack)
 
--- sample/sample.py
+++ sample/sample.py
@@ -5,19 +5,19 @@ import json
 from rabbit_bind import RabbitBinder
 
 
-def handler(msg):
+def handler(msg, client):
     print(msg)
     msg = json.loads(msg)
 
     # Do something with data
     msg['Send'] = True
 
-    return json.dumps(msg)
+    client.send(json.dumps(msg))
 
 
 def main():
     binder = RabbitBinder(host='localhost')
-    binder.bind('input_queue', 'output_queue', handler)
+    binder.bind('news_input_queue', 'news_output_queue', handler)
     binder.start()
     binder.close()
 
